f031e79856cfd0e077e10c764e55121c9ca65a45
Chris Baker <baker@svnetwork.com>
2011-02-18 13:24:27 -0800

    do not detect overlap with self

--- lib/range_validator/validator.rb
+++ lib/range_validator/validator.rb
@@ -1,30 +1,36 @@
 require 'active_model'
+require 'active_support'
 
 module ActiveModel
   module Validations
     class RangeValidator < ActiveModel::EachValidator
+      OPTIONS = [:overlapping, :not_overlapping].freeze
       
       def validate_each(record, attribute, value)
-
         unless value.is_a? Range
            record.errors.add attribute, :not_a_range
            return
         end
-
-        if options[:overlapping]
-          record.send(options[:overlapping]).each do |other_record|
-            record.errors.add attribute, :no_overlap if not OverlapDetector.overlap?(value, other_record.send(attribute))
+        
+        options.slice(*OPTIONS).each do |option, option_value|
+          other_records = record.send(option_value)
+          other_records.reject!{ |other_record| other_record.equal? record }
+          
+          if record.respond_to?(:to_key) and not record.to_key.blank?
+            other_records.reject!{ |other_record| other_record.to_key == record.to_key }
+          end
+          
+          other_records.each do |other_record|
+            overlap = OverlapDetector.overlap? value, other_record.send(attribute)
+            
+            if option == :overlapping and not overlap
+              record.errors.add attribute, :no_overlap
+            elsif option == :not_overlapping and overlap
+              record.errors.add attribute, :overlap
+            end
           end
         end
-
-        if options[:not_overlapping]
-          record.send(options[:not_overlapping]).each do |other_record|
-            record.errors.add attribute, :overlap if OverlapDetector.overlap?(value, other_record.send(attribute))
-          end
-        end
-
       end
-      
     end
   end
 end
--- spec/range_validator_spec.rb
+++ spec/range_validator_spec.rb
@@ -11,56 +11,71 @@ describe "RangeValidator" do
 
   it "should not be valid when the field is not a range" do
     TestModel.validates :name, :range => true
-    model = TestModel.new({:name => "name"})
+    record = TestModel.new({:name => "name"})
 
-    model.should_not be_valid
-    model.errors.should == {:name => ["is not a range"]}
+    record.should_not be_valid
+    record.errors.should == {:name => ["is not a range"]}
   end
 
-  context "overlapping_with" do
+  context "overlapping" do
     
     before(:each) do
-      TestModel.validates :date_range, :range => { :overlapping => :other_models }
+      TestModel.validates :date_range, :range => { :overlapping => :other_records }
     end
     
     it "should be valid when there is overlap" do
-      model1 = TestModel.new({:date_range => @ranges[:january]})
-      model2 = TestModel.new({:date_range => @ranges[:january_to_february], :other_models => [model1]})
+      record1 = TestModel.new({:date_range => @ranges[:january]})
+      record2 = TestModel.new({:date_range => @ranges[:january_to_february], :other_records => [record1]})
 
-      model2.should be_valid
+      record2.should be_valid
     end
     
     it "should not be valid when there is no overlap" do
-      model1 = TestModel.new({:date_range => @ranges[:january]})
-      model2 = TestModel.new({:date_range => @ranges[:february], :other_models => [model1]})
+      record1 = TestModel.new({:date_range => @ranges[:january]})
+      record2 = TestModel.new({:date_range => @ranges[:february], :other_records => [record1]})
 
-      model2.should_not be_valid
-      model2.errors.should == {:date_range => ["does not overlap"]}
+      record2.should_not be_valid
+      record2.errors.should == {:date_range => ["does not overlap"]}
     end
     
   end
 
-  context "not_overlapping_with" do
+  context "not_overlapping" do
     
     before(:each) do
-      TestModel.validates :date_range, :range => { :not_overlapping => :other_models }
+      TestModel.validates :date_range, :range => { :not_overlapping => :other_records }
     end
 
     it "should be valid when there is no overlap" do
-      model1 = TestModel.new({:date_range => @ranges[:january]})
-      model2 = TestModel.new({:date_range => @ranges[:february], :other_models => [model1]})
+      record1 = TestModel.new({:date_range => @ranges[:january]})
+      record2 = TestModel.new({:date_range => @ranges[:february], :other_records => [record1]})
 
-      model2.should be_valid
+      record2.should be_valid
+    end
+    
+    it "should not detect overlap with other records with the same object_id" do
+      record = TestModel.new({:date_range => @ranges[:january]})
+      record.other_records = [record]
+      
+      record.should be_valid
+    end
+    
+    it "should not detect overlap with other records with the same to_key value" do
+      record1 = TestModel.new({:date_range => @ranges[:january], :to_key => [1]})
+      record2 = TestModel.new({:date_range => @ranges[:january], :to_key => [1]})
+      record2.other_records = [record1]
+      
+      record2.should be_valid
     end
 
     it "should not be valid when there is an overlap" do
-      model1 = TestModel.new({:date_range => @ranges[:january]})
-      model2 = TestModel.new({:date_range => @ranges[:january_to_february], :other_models => [model1]})
+      record1 = TestModel.new({:date_range => @ranges[:january]})
+      record2 = TestModel.new({:date_range => @ranges[:january_to_february], :other_records => [record1]})
 
-      model2.should_not be_valid
-      model2.errors.should == {:date_range => ["overlaps"]}
+      record2.should_not be_valid
+      record2.errors.should == {:date_range => ["overlaps"]}
     end
-
+    
   end
 
 end
